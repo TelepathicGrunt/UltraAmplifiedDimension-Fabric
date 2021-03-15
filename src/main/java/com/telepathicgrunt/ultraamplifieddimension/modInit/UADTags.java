4717cc1e185aa538c90b054a5d34d1022a8718db
Alex Sarnese <40846040+TelepathicGrunt@users.noreply.github.com>
2021-03-15 12:03:10 -0400

    Moved to fabric tag hook
    for v9.0.5
    https://github.com/TelepathicGrunt/UltraAmplifiedDimension-Fabric/issues/4

--- src/main/java/com/telepathicgrunt/ultraamplifieddimension/modInit/UADTags.java
+++ src/main/java/com/telepathicgrunt/ultraamplifieddimension/modInit/UADTags.java
@@ -1,8 +1,6 @@
 package com.telepathicgrunt.ultraamplifieddimension.modInit;
 
 import com.telepathicgrunt.ultraamplifieddimension.UltraAmplifiedDimension;
-import com.telepathicgrunt.ultraamplifieddimension.mixin.blocks.BlockTagsAccessor;
-import com.telepathicgrunt.ultraamplifieddimension.mixin.items.ItemsTagsAccessor;
 import net.fabricmc.fabric.api.tag.TagRegistry;
 import net.minecraft.block.Block;
 import net.minecraft.item.Item;
@@ -13,10 +11,10 @@ public class UADTags {
     // All tag wrappers need to be made at mod init.
     public static void tagInit(){}
 
-    public static final Tag.Identified<Item> PORTAL_ACTIVATION_ITEMS = ItemsTagsAccessor.uad_callRegister(UltraAmplifiedDimension.MODID + ":portal_activation_items");
+    public static final Tag<Item> PORTAL_ACTIVATION_ITEMS = TagRegistry.item(new Identifier(UltraAmplifiedDimension.MODID, "portal_activation_items"));
 
-    public static final Tag.Identified<Block> PORTAL_CORNER_BLOCKS = BlockTagsAccessor.uad_callRegister(UltraAmplifiedDimension.MODID + ":portal_corner_blocks");
-    public static final Tag.Identified<Block> PORTAL_NON_CORNER_BLOCKS = BlockTagsAccessor.uad_callRegister(UltraAmplifiedDimension.MODID+":portal_non_corner_blocks");
-    public static final Tag.Identified<Block> TERRACOTTA_BLOCKS = BlockTagsAccessor.uad_callRegister(UltraAmplifiedDimension.MODID+":terracotta");
+    public static final Tag<Block> PORTAL_CORNER_BLOCKS = TagRegistry.block(new Identifier(UltraAmplifiedDimension.MODID, "portal_corner_blocks"));
+    public static final Tag<Block> PORTAL_NON_CORNER_BLOCKS = TagRegistry.block(new Identifier(UltraAmplifiedDimension.MODID, "portal_non_corner_blocks"));
+    public static final Tag<Block> TERRACOTTA_BLOCKS = TagRegistry.block(new Identifier(UltraAmplifiedDimension.MODID, "terracotta"));
     public static final Tag<Block> COMMON_DIRT_BLOCKS = TagRegistry.block(new Identifier("c:dirt"));
 }
